3b025d3e9ac6067d0569c77182b636a809d78c93
cooper <openbilibili@126.com>
2024-02-06 17:42:47 +0800

    chore: update fiile

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,22 @@
 import { resolve } from "path";
-import { defineConfig } from "vite";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 
 export default defineConfig({
+  base: "/m-components/",
+  publicDir: "public",
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
     },
   },
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(), vueJsx(), splitVendorChunkPlugin()],
   server: {
     port: 8080,
   },
   build: {
+    outDir: "dist/m-components",
     rollupOptions: {
       output: {
         manualChunks: {
