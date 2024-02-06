c12d9ed6b78f2328db899a18823e2d078452b115
cooper <openbilibili@126.com>
2024-02-06 16:30:35 +0800

    chore: update file

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,25 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
 
 export default defineConfig({
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src')
-    }
+      "@": resolve(__dirname, "src"),
+    },
   },
   plugins: [vue(), vueJsx()],
   server: {
-    port: 8080
-  }
-})
+    port: 8080,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          lodash: ["lodash"],
+        },
+      },
+    },
+  },
+});
